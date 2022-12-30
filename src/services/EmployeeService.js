5dd3fa8c1a48188843a6957b23f6ffec0a9e772c
Soyeb Ahmed <soyeb88@gmail.com>
2022-12-29 22:33:04 -0500

    finish project

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const EMPLOYEE_API_BASE_URL="http://localhost:8081/api/v1/employees";
+const EMPLOYEE_API_BASE_URL="http://localhost:8081/Project-2/api/v1/employees";
 
 class EmployeeSevice{
     getEmployees(){
