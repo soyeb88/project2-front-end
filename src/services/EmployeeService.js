d47984caa56cafd207af9688d00b445e5da874b6
Soyeb Ahmed <soyeb88@gmail.com>
2022-12-29 23:23:57 -0500

    finish project

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const EMPLOYEE_API_BASE_URL="http://localhost:8081/Project-2/api/v1/employees";
+const EMPLOYEE_API_BASE_URL="http://localhost:8081/rest-api-spring-2022-v1/api/v1/employees";
 
 class EmployeeSevice{
     getEmployees(){
