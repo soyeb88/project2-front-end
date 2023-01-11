214a7c12a94cf21a9ea0be17e2113af5d380b0f6
Soyeb Ahmed <soyeb88@gmail.com>
2023-01-11 14:30:05 -0500

    Bug fix on home routing

--- src/components/HeaderComponents.jsx
+++ src/components/HeaderComponents.jsx
@@ -6,7 +6,7 @@ class HeaderComponents extends Component {
             <div>
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                        <div><a href="http://192.168.1.46.8082/api/v1/employees/">Employee Management App</a></div>
+                        <div><a href="http://localhost:8080/api/v1/employees/">Employee Management App</a></div>
                     </nav>
                 </header>
             </div>
